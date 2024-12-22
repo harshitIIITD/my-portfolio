3a91e1cb94cde88b8b5843a11b6fdaabaf069ad5
harshitIIITD <nhk2harshit@gmail.com>
2024-12-22 08:42:49 +0530

    Enhance ESLint configuration to support React and TypeScript. Added parser options for ECMAScript 2022, integrated React plugin settings, and updated rules to improve code quality. Disabled deprecated React rules for better compatibility with modern React practices.

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,36 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        React: true,
+      },
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      ...tseslint.configs.recommendedTypeChecked.rules,
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    }
+  }
 ];
